e40641e5253b1667043292feadf21030d0219314
Stanislav Zamecnik <stanislav.zamecnik@telekom.com>
2025-10-15 23:06:32 +0200

    bwTS improved according to suggestions

--- R/bwTs.R
+++ R/bwTs.R
@@ -1,12 +1,17 @@
 #' @title Plug-in Method by Tsuruta and Sagae with multiplicative method from Terrell and Scott
 #' 
 #' @description This function computes the optimal smoothing parameter (bandwidth) for circular data
-#' using the plug-in method introduced by Tsuruta and Sagae (see \doi{10.1016/j.spl.2017.08.003}) with the
+#' using the plug-in method, introduced by Tsuruta and Sagae (see \doi{10.1016/j.spl.2017.08.003}) with the
 #' multiplicative method from Terrell and Scott (1980) to form higher-order kernel functions.
+#' The method optimally balances the bias-variance tradeoff by minimizing asymptotic mean 
+#' integrated squared error. 
 #'
 #' @param x Data from which the smoothing parameter is to be computed. The object is
 #'   coerced to a numeric vector in radians using \code{\link[circular]{conversion.circular}}.
 #'   Can be a numeric vector or an object of class \code{circular}.
+#' @param verbose Logical indicating whether to print intermediate computational values
+#'   for debugging purposes. Useful for diagnosing floating-point instability in
+#'   Bessel-based moment terms. Default is FALSE.
 #' 
 #' @details The plug-in approach estimates the optimal bandwidth through the following steps:
 #' \enumerate{
@@ -36,30 +41,35 @@
 #' print(bw)
 #'
 #' x <- rwrappednormal(100, mu = circular(1), rho = 0.7)
+#' x <- as.numeric(x)
 #' bw <- bwTs(x)
 #' print(bw)
 #'
 #' @references
+#' Terrell, George R. & Scott, David W. (1980). On improving convergence rates 
+#' for nonnegative kernel density estimators. \emph{The Annals of Statistics}, 
+#' 8(5):1160--1163.
+#'
 #' Tsuruta, Yasuhito & Sagae, Masahiko (2017). Higher order kernel density
 #' estimation on the circle. \emph{Statistics & Probability Letters}, 131:46--50.
 #' \doi{10.1016/j.spl.2017.08.003}
 #'
 #' @importFrom stats optimize
-#' @import circular
+#' @importFrom circular mle.vonmises, conversion.circular, circular
 #' @import cli
-bwTs <- function(x) {
+bwTs <- function(x, verbose = FALSE) {
   n <- length(x)
   if (n == 0) {
     cli::cli_abort(
       c("{.var x} must be a non-empty object.", "x" = "You've supplied an object of length {n}.")
     )
   }
-  if (!is.numeric(x)) {
+  if (!is.numeric(x) || n < 5) {
     if (all(is.na(x))) {
       cli::cli_abort("{.var x} contains all missing values.")
     }
     cli::cli_abort(
-      c("{.var x} must be a numeric vector.", "x" = "You've supplied a {.cls {class(x)}} vector.")
+      c("{.var x} must be a numeric vector of length at least 5.", "x" = "You've supplied a {.cls {class(x)}} vector.")
     )
   }
   x <- circular(
@@ -77,7 +87,16 @@ bwTs <- function(x) {
   }
 
   n <- length(x)
-  kappa_hat <- circular::mle.vonmises(x)$kappa
+  kappa_hat <- mle.vonmises(x)$kappa
+  
+  if (!is.finite(kappa_hat) || kappa_hat <= 0) {
+    cli::cli_abort(
+      c("MLE estimation of concentration parameter failed.",
+        "x" = "Computed kappa_hat: {.val {kappa_hat}}",
+        "i" = "This may indicate insufficient data or degenerate distribution.")
+    )
+  }
+  
   b0_kappa <- besselI(kappa_hat, 0)
   b0_2kappa <- besselI(2 * kappa_hat, 0)
   b1_2kappa <- besselI(2 * kappa_hat, 1)
@@ -93,9 +112,28 @@ bwTs <- function(x) {
   integral_1 <- (4 * kappa_hat^3 * b1_2kappa - 14 * kappa_hat^2 * b2_2kappa - 3 * kappa_hat^3 * b3_2kappa) / (16 * pi * b0_kappa^2)
   integral_2 <- (36 * kappa_hat^2 * b2_2kappa + 9 * kappa_hat^4 * b2_2kappa + 25 * kappa_hat^3 * b3_2kappa) / (32 * pi * b0_kappa^2)
 
+  if (verbose) {
+    cli::cli_alert_info("Moment terms:")
+    cli::cli_alert_info("  r_fVM2 = {.val {round(r_fVM2, 8)}}")
+    cli::cli_alert_info("  r_fVM3 = {.val {round(r_fVM3, 8)}}")
+    cli::cli_alert_info("  r_fVM4 = {.val {round(r_fVM4, 8)}}")
+    cli::cli_alert_info("  r_fVM2VM1 = {.val {round(r_fVM2VM1, 8)}}")
+    cli::cli_alert_info("  integral_1 = {.val {round(integral_1, 8)}}")
+    cli::cli_alert_info("  integral_2 = {.val {round(integral_2, 8)}}")
+  }
+
   r_hat <- 1/4 * r_fVM2VM1 + 25/16 * r_fVM2 - 5/4 * r_fVM3 + 1/4 * r_fVM4 - 5/4 * integral_1 - 1/2 * integral_2
 
-  bw <- (288 / (33 - 16 * sqrt(2/5)) * r_hat * n)^(2/9)
+  if (!is.finite(r_hat) || r_hat <= 0) {
+    cli::cli_abort(
+      c("Computed functional R-hat is invalid for bandwidth calculation.",
+        "x" = "Computed r_hat: {.val {r_hat}}",
+        "i" = "This may indicate numerical instability or inappropriate data distribution.")
+    )
+  }
+
+  c_ts <- 288 / (33 - 16 * sqrt(2/5))
+  bw <- (c_ts * r_hat * n)^(2/9)
 
   return(bw)
 }
