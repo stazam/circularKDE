1658d1665937217e073e53fa6e11772093acda55
Stanislav Zamecnik <stanislav.zamecnik@telekom.com>
2025-10-18 21:18:20 +0200

    updates of utils script

--- R/utils.R
+++ R/utils.R
@@ -3,7 +3,7 @@ localFactor <- function(x,
                          alpha = 0.5,
                          type = c("am", "gm", "rv", "n")) {
   type <- match.arg(type)
-  lambdas <- sapply(x,kernelDensityEst, x = x, bw = bw0)
+  lambdas <- vapply(x, function(z_i) kernelDensityEst(z_i, x = x, bw = bw0), numeric(1))
   g <- switch(
     type,
     "am" = mean(lambdas),
@@ -15,14 +15,14 @@ localFactor <- function(x,
     "rv" = max(lambdas) - min(lambdas),
     "n" = 1
   )
-  result <- (lambdas / g) ^ (-alpha)
+  result <- (g / lambdas) ^ (alpha)
   return(result)
 }
 
 kernelDensityEst <- function(z, x, bw) {
   n <- length(x)
   factor <- 1 / (2 * n * pi * besselI(bw, 0))
-  result <- factor * sum(exp (bw * cos(z - x)))
+  result <- factor * rowSums(exp(bw * cos(outer(z, x, "-"))))
   return(result)
 }
 
